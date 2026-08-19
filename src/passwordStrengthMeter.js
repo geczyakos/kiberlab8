fd485210d5722b7ed28058c8b62fde1a594f84bd
geczyakos <geczyakos@gmail.com>
2026-08-19 10:06:08 +0200

    feat: add password strength meter

---
+++ src/passwordStrengthMeter.js
@@ -0,0 +1,8 @@
+export function scoreStrength(password = '') {
+  let score = 0;
+  if (password.length >= 10) score += 1;
+  if (/[A-Z]/.test(password) && /[a-z]/.test(password)) score += 1;
+  if (/[0-9]/.test(password)) score += 1;
+  if (/[^A-Za-z0-9]/.test(password)) score += 1;
+  return score;
+}
